c4069b3cf7b23e4ce82425a0907b7de7cacf44ad
heptacode <admin@hyunwoo.kim>
2022-10-07 11:01:45 +0900

    Combine baseUrl & path

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,44 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 /**
  * HTTP GET Request
- * @param baseUrl
- * @param path
+ * @param url
  * @param query Optional
  * @param config Optional
  * @returns AxiosResponse
  */
-export function getRequest<T = any>(baseUrl: string, path: string, query?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-  return axios.get<T>(`${baseUrl}${path}${query ? `?${String(new URLSearchParams(query))}` : ''}`, config);
+export function getRequest<T = any>(url: string, query?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+  return axios.get<T>(`${url}${query ? `?${String(new URLSearchParams(query))}` : ''}`, config);
 }
 
 /**
  * HTTP POST Request
- * @param baseUrl
- * @param path
+ * @param url
  * @param body Optional
  * @param config Optional
  * @returns AxiosResponse
  */
-export function postRequest<T>(baseUrl: string, path: string, body?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-  return axios.post<T>(`${baseUrl}${path}`, body, config);
+export function postRequest<T>(url: string, body?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+  return axios.post<T>(`${url}`, body, config);
 }
 
 /**
  * HTTP PUT Request
- * @param baseUrl
- * @param path
+ * @param url
  * @param body Optional
  * @param config Optional
  * @returns AxiosResponse
  */
-export function putRequest<T>(baseUrl: string, path: string, body?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-  return axios.put<T>(`${baseUrl}${path}`, body, config);
+export function putRequest<T>(url: string, body?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+  return axios.put<T>(`${url}`, body, config);
 }
 
 /**
  * HTTP DELETE Request
- * @param baseUrl
- * @param path
+ * @param url
  * @param query Optional
  * @param config Optional
  * @returns AxiosResponse
  */
-export function deleteRequest<T = any>(baseUrl: string, path: string, query?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-  return axios.delete<T>(`${baseUrl}${path}${query ? `?${String(new URLSearchParams(query))}` : ''}`, config);
+export function deleteRequest<T = any>(url: string, query?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+  return axios.delete<T>(`${url}${query ? `?${String(new URLSearchParams(query))}` : ''}`, config);
 }
